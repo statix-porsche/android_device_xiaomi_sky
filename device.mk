77536be8bd63c1dc97a8fd900a2cf4193acb7328
redducc <sushmit718@gmail.com>
2024-01-27 08:29:08 +0000

    sky: Switch to vendor/hardware/xiaomi

--- device.mk
+++ device.mk
@@ -447,7 +447,7 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
-    hardware/xiaomi
+    vendor/hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
